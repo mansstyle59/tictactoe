3337b1ee47415ad3f255084c056553cf2fb1b109
mansstyle59 <138115513+mansstyle59@users.noreply.github.com>
2026-04-16 13:01:00 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // IMPORTANT: remplacez 'tictactoe' par le nom exact de votre repo GitHub
-  base: '/tictactoe/',
 })
